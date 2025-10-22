375c7762a20bdf2de8d580013f803766c7c80749
Isak <isak.fogge1@gmail.com>
2025-10-22 17:05:55 +0200

    feat: FlashCard component

---
+++ src/lib/components/FlashCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    
+    let { question = "This is a question", explanation = "This is an explanation for a question"} = $props();
+    let show_explanation = $state(false);
+
+    function flip_card() {
+        show_explanation = !show_explanation;
+    }
+</script>
+
+<button 
+class=flashcard
+onclick={flip_card}
+>
+    {#if show_explanation}
+        <p>{explanation}</p>
+    {:else}
+        <p>{question}</p>
+    {/if}
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import FlashCard from "$lib/components/FlashCard.svelte";
+
+</script>
+
+<FlashCard />
